311ad4ccb396dcff674375a0f015ddc8e3058e9d
Mazhabsho <smazhabsho@gmail.com>
2023-02-21 16:24:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+#Hool React;
+
+ In React, the state is data or properties you can use in our application. States are mutable, meaning their
+ value can change, and for that, the useState() hook is used to handle and manage your states. The useState()
+  hook takes in the initial value of the state variable as an argument. This value could be of any data type, 
+  such as string, number, object, array, and lots more.
+  ![](img1)
+
+  It is essential to know that the useState() hook does not return just a variable, as seen above, but it also
+   returns a second value, a function that you can use to update the state value.
+   ![](img2)
+
